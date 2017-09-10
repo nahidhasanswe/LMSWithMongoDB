2be003356dbe03923b186dce911d3284b7d35939
Nahid Hasan <nahid675@diu.edu.bd>
2017-09-11 00:09:45 +0600

    Unneccessary

--- LMSWithMongoDB/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace LMSWithMongoDB.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- LMSWithMongoDB/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace LMSWithMongoDB.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
